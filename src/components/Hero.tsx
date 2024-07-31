2209b45c1fcf8cbcb127fbcc3b959342680164dc
jonathan_baraza <jbaraza46@gmail.com>
2024-07-31 21:16:38 +0300

    feat: hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="w-full bg-blue-700 h-[40vh] text-white flex flex-col justify-center items-center gap-4 p-16 border-t border-t-[#c0dfe2]">
+      <div className="text-5xl font-bold">
+        Connecting Talent with Opportunity
+      </div>
+      <div className="text">
+        Discover Endless Opportunities and Unlock Your Potential.
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
