b6d8488c122e1abc12b0feabeb50ebf9424fd0e6
jonathan_baraza <jbaraza46@gmail.com>
2024-07-31 21:30:29 +0300

    feat: props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+
+
+
 function App() {
+  const heroData={
+    title: "Connecting Talent with Opportunity",
+    tagline: " Discover Endless Opportunities and Unlock Your Potential",
+  }
+  
   return (
     <>
       <div>
         <Navbar />
-        <Hero/>
+        <Hero title={heroData.title} tagline={heroData.tagline}/>
       </div>
     </>
   );
