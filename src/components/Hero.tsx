040de74cd85607faf386841cf9b2d25b7ab662b0
jonathan_baraza <jbaraza46@gmail.com>
2024-07-31 21:30:41 +0300

    feat: hero section completed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-
-const Hero = () => {
+type HeroProps = {
+  title: string;
+  tagline: string;
+};
+const Hero = ({title,tagline}:HeroProps) => {
   return (
     <div className="w-full bg-blue-700 h-[40vh] text-white flex flex-col justify-center items-center gap-4 p-16 border-t border-t-[#c0dfe2]">
-      <div className="text-5xl font-bold">
-        Connecting Talent with Opportunity
-      </div>
-      <div className="text">
-        Discover Endless Opportunities and Unlock Your Potential.
-      </div>
+      <div className="text-5xl font-bold">{title}</div>
+      <div className="text">{tagline}</div>
     </div>
   );
 };
