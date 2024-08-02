4c5f1fc24e67906024857f0b0e6ff6e1d235386c
jonathan_baraza <jbaraza46@gmail.com>
2024-08-02 23:27:48 +0300

    feat: jobs list

--- src/components/JobListings.tsx
+++ src/components/JobListings.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import jobs from "../jobs.json";
+import JobCard from "./JobCard";
 
 const JobListings = () => {
-  return <div className="w-full min-h-[30vh] p-6">
-    {jobs.jobs.map((job)=><div key={job.id}>{job.title}</div>)}
-
-  </div>;
+  console.log(jobs.jobs[0]);
+  return (
+    <div className="w-full py-12 my-12 bg-[#ebf4f5] min-h-[30vh]">
+      <div className="w-3/4   mx-auto  grid grid-cols-3 gap-8  ">
+        {jobs.jobs.map((job) => (
+          <JobCard job={job} key={job.id} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default JobListings;
