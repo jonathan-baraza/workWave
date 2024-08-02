0aa406de4120282bbb895d4af1f8798a19801c8e
jonathan_baraza <jbaraza46@gmail.com>
2024-08-02 23:03:53 +0300

    more components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-
 import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
 import Navbar from "./components/Navbar";
 
-
-
 function App() {
-  const heroData={
+  const heroData = {
     title: "Connecting Talent with Opportunity",
     tagline: " Discover Endless Opportunities and Unlock Your Potential",
-  }
-  
+  };
+
   return (
     <>
       <div>
         <Navbar />
-        <Hero title={heroData.title} tagline={heroData.tagline}/>
+        <Hero title={heroData.title} tagline={heroData.tagline} />
+        <HomeCards />
+        <JobListings />
       </div>
     </>
   );
