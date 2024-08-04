47e47b7e2e7818ac8e4481758850565636f75485
jonathan_baraza <jbaraza46@gmail.com>
2024-08-04 21:16:51 +0300

    feat: jobs loader

--- src/components/JobListings.tsx
+++ src/components/JobListings.tsx
@@ -1,16 +1,33 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import jobs from "../jobs.json";
 import JobListing from "./JobListing";
-
+import { JobType } from "./JobListing";
 const JobListings = () => {
-  console.log(jobs.jobs[0]);
+  const [viewJobs, setViewJobs] = useState<JobType[]>(jobs.jobs.slice(0, 3));
+  const [jobRange, setJobRange] = useState<number>(2);
+
+  //Set Jobs
+  useEffect(() => {
+    setViewJobs(jobs.jobs.slice(0, jobRange));
+  }, [jobRange]);
+
   return (
     <div className="w-full py-12 my-12 bg-[#ebf4f5] min-h-[30vh]">
-      <div className="w-3/4   mx-auto  grid grid-cols-3 gap-8  ">
-        {jobs.jobs.map((job) => (
-          <JobListing job={job} key={job.id} />
-        ))}
+      <div className=" w-[85%] md:w-3/4   mx-auto  grid grid-cols-1 md:grid-cols-3 gap-8  ">
+        {viewJobs?.length > 0 &&
+          viewJobs.map((job: JobType) => <JobListing job={job} key={job.id} />)}
       </div>
+
+      {jobs.jobs.length > jobRange && (
+        <div className="w-full flex justify-center my-5">
+          <button
+            onClick={() => setJobRange(jobRange + 3)}
+            className="w-fit text-sm bg-black hover:cursor-pointer hover:shadow-xl duration-100 text-white rounded py-1 px-3"
+          >
+            View More
+          </button>
+        </div>
+      )}
     </div>
   );
 };
