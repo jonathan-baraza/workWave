9066ddff1ead9ebf03acfd6252bda6c0d8d57460
jonathan_baraza <jbaraza46@gmail.com>
2024-08-04 22:17:14 +0300

    feat: react routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+import ViewAllJobs from "./pages/ViewAllJobs";
 import Homepage from "./pages/Homepage";
+import MainLayout from "./layouts/MainLayout";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path={"/"} element={<MainLayout/>}>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/jobs" element={<ViewAllJobs />} />
+      <Route path="/about" element={<div>About page</div>} />
+    </Route>
+  )
+);
 
 function App() {
-  return <>
-  <Homepage/></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
