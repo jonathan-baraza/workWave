376b0f4f6ff7198b628bc47215df258411b262dd
jonathan_baraza <jbaraza46@gmail.com>
2024-08-04 21:22:46 +0300

    error fix

--- src/components/JobListings.tsx
+++ src/components/JobListings.tsx
@@ -13,6 +13,9 @@ const JobListings = () => {
 
   return (
     <div className="w-full py-12 my-12 bg-[#ebf4f5] min-h-[30vh]">
+      <div className="flex justify-center pb-8 px-3">
+        <div className="text-blue-700 font-bold text-2xl">Browse Jobs</div>
+      </div>
       <div className=" w-[85%] md:w-3/4   mx-auto  grid grid-cols-1 md:grid-cols-3 gap-8  ">
         {viewJobs?.length > 0 &&
           viewJobs.map((job: JobType) => <JobListing job={job} key={job.id} />)}
