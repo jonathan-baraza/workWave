5b78abae597624f1e552b70afb7b7a9b03cc867b
jonathan_baraza <jbaraza46@gmail.com>
2024-08-04 21:23:05 +0300

    feat: new components

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Hero from "./components/Hero";
 import HomeCards from "./components/HomeCards";
 import JobListings from "./components/JobListings";
 import Navbar from "./components/Navbar";
+import ViewAllJobs from "./components/ViewAllJobs";
 
 function App() {
   const heroData = {
@@ -16,6 +17,7 @@ function App() {
         <Hero title={heroData.title} tagline={heroData.tagline} />
         <HomeCards />
         <JobListings />
+        {/* <ViewAllJobs /> */}
       </div>
     </>
   );
